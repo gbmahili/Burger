b51b67b0bf800ba2f2f5dc05ef6c8e1f2f6b1397
gbmahili <georgesmahili@gmail.com>
2018-02-16 18:23:06 -0500

    Added connection.js

---
+++ config/connection.js
@@ -0,0 +1,25 @@
+var mysql = require("mysql");
+
+var connection;
+if (process.env.JAWSDB_URL) {
+    //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    //local host
+    connection = mysql.createConnection({
+        root: 3306,
+        host: "localhost",
+        user: "root",
+        password: "YOUR_DB_PASSWORD",
+        database: "burgers_db",
+    });
+};
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+// Export connection for our ORM to use.
+module.exports = connection;
